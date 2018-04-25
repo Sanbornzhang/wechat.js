ea88d9e3083d93a245798975c727fefa84bf78ba
SanbornZhang <z568817749@gmail.com>
2018-04-25 20:54:20 +0800

    add unifiedOrder func

--- index.js
+++ index.js
@@ -1,46 +1,73 @@
 const fs = require('fs')
 const crypto = require('crypto')
+const process = require('process')
+const { URL } = require('url')
+const clone = require('clone')
+const xml2js = require('xml2js')
+const request = require('superagent')
+
 class WeChat{
-  constructor(aOptions){
-    this.appid = aOptions.appid
-    this.mch_id = aOptions.mch_id
-    this.apiKey = aOptions.apiKey
-    this.signType = aOptions.signType || 'md5'
+  constructor(options){
+    this.appid = options.appid
+    this.mch_id = options.mch_id
+    this.apiKey = options.apiKey
+    this.signType = options.signType || 'md5'
+    this.env = options.env || process.env.NODE_ENV && 
+                              process.env.NODE_ENV === 'production' ? 'production' : 'sandbox'
   }
-  set(aOptions){
+  set(options){
     // notify_url,trade_type,pfx
-    Object.keys(aOptions)
-    .forEach((vKey)=>{
-      if (vKey === 'pfx'){
-        this[vKey] = fs.readFileSync(aOptions[vKey], 'utf8')
+    const that = clone(this)
+    Object.keys(options)
+    .forEach((key)=>{
+      if (key === 'pfx'){
+        that[key] = fs.readFileSync(options[key], 'utf8')
       }
       else{
-        this[vKey] = aOptions[vKey]
+        that[key] = options[key]
       }
     })
+    return that
   }
-  sign(encoding = 'utf8', outputFormat = 'base64'){
-    // 获取支持的算法
-    const vSignStr = buildSignStr(this)
+  sign(data,encoding = 'utf8', outputFormat = 'base64'){
+    // 获取支持的算法 md5 以及 	sha256 ref: https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=4_3
+    const signStr =  buildSignStr(data)
     const supportedSign = crypto.getHashes()
     if(supportedSign.indexOf(this.signType) === -1 ){
       const vError = new Error()
       vError.message = 'OS not support this algorithm'
       return vError
     }
-    const signer = crypto.createSign(aSignType)
-    signer.update(aSignStr, encoding)
-    signer.sign(this.apiKey, 'base64')
-    return signer
+    const signer =  crypto.createHmac(this.signType, sharedSecret)
+                    .update(signStr)
+                    .digest("hex");
+    return signer.toUpperCase()
   }
+  baseUrl (){
+    const baseUrl = 'https://api.mch.weixin.qq.com/'
+    const baseSandBoxUrl = 'https://api.mch.weixin.qq.com//sandboxnew/'
+    return this.env === 'production' ? baseUrl : baseSandBoxUrl
+
+  }
+  unifiedOrder(data){
+    const unifiedOrderUrl = new URL( '/pay/unifiedorder', this.baseUrl())
+    const builder = new xml2js.Builder();
+    return builder.buildObject(data)
+    // request.post(unifiedOrderUrl.href) 
+    // .send((builder))
+  }
+
 }
-const buildSignStr = (aObj)=>{
-  return Object.keys(aObj)
+const buildSignStr = (obj)=>{
+  const limitKey = ['sign']
+  const signStr = Object.keys(aObj)
   .filter((key)=>{
-    return aObj[key]!== null && aObj[key]!== undefined && aObj[key]!== ''
+    return obj[key]!== null && obj[key]!== undefined && obj[key]!== '' && limitKey.indexOf(key) === -1
   })
   .sort()
   .map((key)=>{
     return `${key}=${aObj[key]}`
   }).join('&')
+  return `${signStr}key=${this.apiKey}`
 }
+
